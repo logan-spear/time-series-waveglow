de373e93dd0516d1b3ea724484b2055282064aa9
Daniel Salz <dasalz@stanford.edu>
2019-12-13 21:10:11 -0800

    update

--- training/train.py
+++ training/train.py
@@ -15,16 +15,12 @@ def check_threads(threads):
 	
 	return count
 
-# def run_new(args):
-	# os.system('python3 train.py %s' % args)
 with open("./training_config_lists/config_list_2_of_10", "rb") as fp:
 	test_list = pickle.load(fp)
 
 
 threads = []
 max_threads = 1
-
-
 
 epochs=250
 batch_size=120
@@ -45,31 +41,3 @@ validation_patience=10
 while(len(test_list) > 0):
 	config = test_list.pop()
 	run_training(config, epochs, batch_size, seed, generate_per_epoch, generate_final_plots, checkpointing, use_gpu, n_channels, n_context_channels, rolling, datafile, valid_split, small_subset, validation_patience)
-
-# for t in range(max_threads):
-#   config = test_list.pop()
-#   threads.append(threading.Thread(target=run_training, args=(config, epochs, batch_size, seed, generate_per_epoch, generate_final_plots, checkpointing, use_gpu, n_channels, n_context_channels, rolling, datafile, valid_split, small_subset, validation_patience,)))
-
-#   threads[-1].start()
-
-# print("Finished starting %d threads" % max_threads)
-
-# while(len(test_list) > 0):
-#   count = check_threads(threads)
-#   if count > 0:
-#       for c in range(count):
-#           if len(test_list) == 0: break
-		
-#           # threads.append(Thread(target=run_new, args=(new_args,)))
-#           # print("Starting thread for node %d" % next_node)
-#           # threads[-1].start()
-#           # num_tests-=1
-#           config = test_list.pop()
-#           threads.append(threading.Thread(target=run_training, args=(config, epochs, batch_size, seed, generate_per_epoch, generate_final_plots, checkpointing, use_gpu, n_channels, n_context_channels, rolling, datafile, valid_split, small_subset, validation_patience,)))
-
-#           threads[-1].start()
-#   else:
-#       continue
-		
-# for t in threads:
-#   t.join()
